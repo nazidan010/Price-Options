e7e67d38b5f6ee2ee1be8e42a2f30b405660eae0
nazidan010 <nazidan010@gmail.com>
2024-12-04 19:00:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-7xl font-bold bg-rose-700 text-center'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
